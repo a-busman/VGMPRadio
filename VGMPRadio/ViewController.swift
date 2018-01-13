5f7e83c262a4aecd4df63245582bfd8828f2595b
Alex Busman <abusman@taser.com>
2018-01-12 20:44:14 -0800

    Initial Commit

---
+++ VGMPRadio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VGMPRadio
+//
+//  Created by Alex Busman on 1/12/18.
+//  Copyright © 2018 Alex Busman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
